a56c1b18e801b0722f266f61fe85e56160e99b83
abhishek choudhury <mail2abhic@gmail.com>
2016-09-05 19:47:31 +0530

    Removed use of 'q' module

--- app/modules/db/services/enterpriseService.js
+++ app/modules/db/services/enterpriseService.js
@@ -1,21 +1,21 @@
 var mongoose = require('mongoose');
 var Enterprise = require('../models/enterprise');
-var Q = require('q');
+//var Q = require('q');
 
-var createEnterprise = function (requestData) {
+var createEnterprise = function(requestData) {
     console.log("Enter into insert Enterprise method " + JSON.stringify(requestData));
     var objToinsert = new Enterprise(requestData);
 
     console.log(objToinsert);
     //Saving the model instance to the DB
-    objToinsert.save(function (err, docsInserted) {
+    objToinsert.save(function(err, docsInserted) {
         if (err) throw err;
         console.log("Enterprise Saved Successfully " + docsInserted);
     });
 }
 
-var queryEnterprise = function (fieldName,fieldValue) {
-    console.log("Enter into query enterprise method with fieldName : " + fieldName + " and fieldValue : "+fieldValue);
+var queryEnterprise = function(fieldName, fieldValue) {
+    console.log("Enter into query enterprise method with fieldName : " + fieldName + " and fieldValue : " + fieldValue);
     //Now querying those books which have less than 100 pages
     //Find API takes in a query condition, attributes in the document to be projected,
     //callback to be executed when the data is available.
