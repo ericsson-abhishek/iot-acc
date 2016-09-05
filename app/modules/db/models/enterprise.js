3a9e3f4ee26b75094349c687b44f3ad44249a420
abhishek choudhury <mail2abhic@gmail.com>
2016-09-05 19:38:55 +0530

    Updated Model for enterprise

--- app/modules/db/models/enterprise.js
+++ app/modules/db/models/enterprise.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 var enterpriseSchema = new mongoose.Schema({
-  username:{type : String, unique : true, required : true, dropDups : true},
-  password:String,
-  email:{type : String, unique : true, required : true, dropDups : true}
+    username: { type: String, unique: true, required: true, dropDups: true, lowercase: true, trim: true },
+    password: String,
+    email: { type: String, unique: true, required: true, dropDups: true, lowercase: true, trim: true }
 });
 
-module.exports=mongoose.model('Enterprise', enterpriseSchema,"enterprise_details");
+module.exports = mongoose.model('Enterprise', enterpriseSchema, "enterprise_details");
--- app/modules/db/test/enterpriseServiceTest.js
+++ app/modules/db/test/enterpriseServiceTest.js
@@ -2,8 +2,7 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/iotaccdb');
 
 var enterpriseService = require("../services/enterpriseService");
-var enterpriseData = {number:"cus101",username:"Avishek",password:"Avishek",email:"abhishek@choudhury.com"};
+var enterpriseData = { username: "sahoo", password: "Avishek", email: "abhishek@choudhury.com" };
 
-//enterpriseService.createEnterprise(enterpriseData);
-enterpriseService.queryEnterpriseByEmail("abhishek@choudhury.com");
-
+enterpriseService.createEnterprise(enterpriseData);
+//enterpriseService.queryEnterpriseByEmail("abhishek@choudhury.com");
