70632f1fca6764111a8133b4b3d050e5cc28e12c
Sudarsan Sahoo <sudarsan.sahoo@ericsson.com>
2016-09-05 19:30:24 +0530

    Added for unique constraint in enterprise.js

--- app/modules/db/models/enterprise.js
+++ app/modules/db/models/enterprise.js
@@ -1,10 +1,9 @@
 var mongoose = require('mongoose');
 
 var enterpriseSchema = new mongoose.Schema({
-  number:String,  
-  username:String,
+  username:{type : String, unique : true, required : true, dropDups : true},
   password:String,
-  email:String
+  email:{type : String, unique : true, required : true, dropDups : true}
 });
 
 module.exports=mongoose.model('Enterprise', enterpriseSchema,"enterprise_details");
