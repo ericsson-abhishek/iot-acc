e89d910fd5dfff392411a397662f8724a94189e9
abhishek choudhury <mail2abhic@gmail.com>
2016-09-05 10:45:28 +0530

    Added relave URI for different environments

--- app/modules/simulator/client_http.js
+++ app/modules/simulator/client_http.js
@@ -1,31 +1,39 @@
 var rp = require('request-promise');
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+var urlPref = '';
+var env = process.env.NODE_ENV || 'development';
+if (env === 'production') {
+    urlPref = 'https://iot-one.herokuapp.com';
+
+} else {
+    urlPref = 'http://localhost:9000'
 }
 
-function sendStatus(deviceId)
-{
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function sendStatus(deviceId) {
 
     console.log("sendStatus is called");
     var options = {
         method: 'POST',
-        uri: 'http://localhost:9000/devices/status/'+deviceId,
+        uri: urlPref + '/devices/status/' + deviceId,
         body: {
-            data:getRandomInt(10,20),
-            deviceId:deviceId,
+            data: getRandomInt(10, 20),
+            deviceId: deviceId,
             time: new Date()
         },
         json: true // Automatically stringifies the body to JSON 
     };
 
     rp(options)
-        .then(function (parsedBody) {
+        .then(function(parsedBody) {
             console.log(parsedBody)
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.log(err);
             throw err;
         });
