a9bc2ec8a2c8511d4da3197546fe4fe68ee5f53f
Sudarsan Sahoo <sudarsan.sahoo@ericsson.com>
2016-09-09 16:47:37 +0530

    Added for foreign key constraints"

--- app/modules/db/models/device.js
+++ app/modules/db/models/device.js
@@ -1,13 +1,14 @@
 var mongoose = require('mongoose');
 //Used for unique validator
 var uniqueValidator = require('mongoose-unique-validator');
+var Enterprise = require('./enterprise');
 
 var deviceSchema = mongoose.Schema({
   name: {type: String, required: true, dropDups: true, lowercase: true, trim: true },
   manufacturer: { type: String, required: true, dropDups: true, lowercase: true, trim: true },
   serialNo: {type: String, unique: true, required: true, dropDups: true, lowercase: true, trim: true },
   protocol : {type: String, required: true, dropDups: true, lowercase: true, trim: true },
-  enterpriseId:{type: String, required: true, dropDups: true, lowercase: true, trim: true },
+  enterpriseId:[{type: mongoose.Schema.Types.ObjectId, ref: 'Enterprise'}],
   status : {type : Boolean , required: true, dropDups: true, lowercase: true, trim: true}
   //enterpriseEmail:{type: mongoose.Schema.Types.ObjectId, ref: 'Enterprise'}
 });
