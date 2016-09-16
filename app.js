5d9b45f82170a0a04131fd032f0eb27f04a76a00
abhishek choudhury <mail2abhic@gmail.com>
2016-09-16 13:37:19 +0530

    changes for Heroku have been added

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ var PORT = process.env.PORT || 9099;
 app.use(bodyParser.json());
 
 // create application/x-www-form-urlencoded parser 
-var urlencodedParser = bodyParser.urlencoded({extended: false})
-// to be replaced with Mongo DB
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+    // to be replaced with Mongo DB
 var hashMap = require('hashmap');
 
 var deviceDB = new hashMap();
@@ -54,10 +54,10 @@ var deviceDB = new hashMap();
 
 var io = require('socket.io')(http);
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
     console.log("connection initiated from " + (socket.toString()));
 
-    socket.on('message', function (message) {
+    socket.on('message', function(message) {
         console.log("message received " + message.text);
         socket.broadcast.emit('message', message);
     });
@@ -69,12 +69,12 @@ var UniqueNumber = 0;
 //Enterprise get method
 
 
-app.get("/about", function (req, resp) {
+app.get("/about", function(req, resp) {
     resp.send("About items are listed here");
 });
 
 
-app.post("/device", authentication.authenticateRequired, function (req, resp) {
+app.post("/device", authentication.authenticateRequired, function(req, resp) {
     //console.log('Enter into device create method' + JSON.stringify(req.body, null, 2));
     //getting enterprise id from jwt service
     //JSON.parse() removes double quotes of json keys
@@ -90,35 +90,35 @@ app.post("/device", authentication.authenticateRequired, function (req, resp) {
     //console.log(req.body.enterpriseId);
     //console.log("Request data after adding enterprise id : "+JSON.stringify(req.body, null, 2));
     //calling create device method with request data
-    appdb.deviceService.createDevice(deviceData).then(function (result) {
+    appdb.deviceService.createDevice(deviceData).then(function(result) {
         //Sending response back on successfull device creation
         resp.status(200).send("Device is created successfully with Device Id :" + result.get("_id"));
         //Error handling when some problem occurs for device creation
-    }).catch(function (err) {
+    }).catch(function(err) {
         console.log(err);
         resp.status(500).send("Sorry !!!! Some error occured while creating device.Please try again.");
     });
 });
 
-app.get("/device", authentication.authenticateRequired, function (req, resp) {
+app.get("/device", authentication.authenticateRequired, function(req, resp) {
     //calling query device method 
-    appdb.deviceService.queryDeviceByFilter("enterpriseId", req.params.enterpriseId).then(function (result) {
+    appdb.deviceService.queryDeviceByFilter("enterpriseId", req.params.enterpriseId).then(function(result) {
         //Sending response back on successfull device creation
         resp.status(200).send(result);
         //Error handling when some problem occurs for device creation
-    }).catch(function (err) {
+    }).catch(function(err) {
         console.log(err);
         resp.status(404).send("Sorry !!!! No devices are registered for enterprise : " + req.query.enterpriseEmail);
     });
 });
 
-app.post("/enterprise", function (req, resp) {
+app.post("/enterprise", function(req, resp) {
 
     var enterpriseData = _.omit(req.body, '');
     console.dir(enterpriseData);
 
     //calling create enterprise method with request data
-    appdb.enterpriseService.createEnterprise(enterpriseData).then(function (result) {
+    appdb.enterpriseService.createEnterprise(enterpriseData).then(function(result) {
 
         // creating object for sending the activation mail notification
         var mailObj = {
@@ -128,34 +128,34 @@ app.post("/enterprise", function (req, resp) {
         };
 
         // send the mail notification
-        mailSender.send(mailObj).then(function (response) {
-            console.log(response);
-            resp.status(200).send(_.omit(result, '_id'));
-        })
-        //resp.status(200).send(_.omit(result, '_id'));
-        //Sending response back on successfull device creation
+        mailSender.send(mailObj).then(function(response) {
+                console.log(response);
+                resp.status(200).send(_.omit(result, '_id'));
+            })
+            //resp.status(200).send(_.omit(result, '_id'));
+            //Sending response back on successfull device creation
 
         //Error handling when some problem occurs for device creation
-    }).catch(function (err) {
+    }).catch(function(err) {
         console.log(err);
         resp.status(500).send("Sorry !!!! Some error occured while registered.Please try again.");
     });
 });
 
-app.get("/enterprise/activate", function (req, resp) {
+app.get("/enterprise/activate", function(req, resp) {
     var activateId = req.query.activateId;
     console.log("activate is getting called for " + activateId);
-    appdb.enterpriseService.activateEnterprise(activateId).then(function (result) {
+    appdb.enterpriseService.activateEnterprise(activateId).then(function(result) {
         //console.log(result);
         resp.status(200).send(result);
-    }).catch(function (err) {
+    }).catch(function(err) {
         console.log("Some error occurred while enterprise activation" + err);
         resp.status(500).send(err);
     });
 });
 
 app.post("/devices/status/:deviceId",
-    function (req, resp) {
+    function(req, resp) {
         var deviceId = req.params.deviceId;
         console.log("status received from  " + deviceId);
         console.dir(req.body);
@@ -164,10 +164,10 @@ app.post("/devices/status/:deviceId",
     });
 
 app.post("/devices/activate/:deviceId",
-    function (req, resp) {
+    function(req, resp) {
         var deviceId = req.params.deviceId;
         console.log("activate is getting called for " + deviceId)
-        var interval = setInterval(function () {
+        var interval = setInterval(function() {
             clientSim.sendStatus(deviceId)
         }, 2000);
         deviceDB.set(deviceId, interval);
@@ -176,7 +176,7 @@ app.post("/devices/activate/:deviceId",
     });
 
 app.post("/devices/deactivate/:deviceId",
-    function (req, resp) {
+    function(req, resp) {
         var deviceId = req.params.deviceId;
         console.log("deactivate device is getting called for " + deviceId)
         var interval = deviceDB.get(deviceId); //setInterval(() => { clientSim.sendStatus(deviceId) }, 2000);
@@ -188,7 +188,7 @@ app.post("/devices/deactivate/:deviceId",
     });
 
 app.post("/devices/create",
-    function (req, resp) {
+    function(req, resp) {
         UniqueNumber += 1;
         var deviceId = devicePrefix + UniqueNumber;
         console.log("generated device id " + deviceId);
@@ -196,37 +196,37 @@ app.post("/devices/create",
     });
 
 
-app.get("/enterprise", authentication.authenticateRequired, function (req, resp) {
-    appdb.enterpriseService.queryEnterpriseByFilter("_id", req.params.enterpriseId).then(function (result) {
-        console.log("get enterprise");
-        console.dir(result)
-        console.log("get enterprise result");
-        appdb.deviceService.queryDeviceByFilter("enterpriseId", result._id).then(function (devices) {
-            var responseData = _.pick(result, 'firstname', 'lastname');
-            responseData.totalDevices = devices.length;
-            var activatedDevices = 0;
-            for (var i = 0; i < devices.length; i++) {
-                if (devices[i].status === true) {
-                    activatedDevices += 1;
+app.get("/enterprise", authentication.authenticateRequired, function(req, resp) {
+    appdb.enterpriseService.queryEnterpriseByFilter("_id", req.params.enterpriseId).then(function(result) {
+            console.log("get enterprise");
+            console.dir(result)
+            console.log("get enterprise result");
+            appdb.deviceService.queryDeviceByFilter("enterpriseId", result._id).then(function(devices) {
+                var responseData = _.pick(result, 'firstname', 'lastname');
+                responseData.totalDevices = devices.length;
+                var activatedDevices = 0;
+                for (var i = 0; i < devices.length; i++) {
+                    if (devices[i].status === true) {
+                        activatedDevices += 1;
+                    }
                 }
-            }
-            responseData.activatedDevices = activatedDevices;
-            console.log("Activated devices  " + activatedDevices);
-            resp.header("Authorization", req.params.token).status(200).send(responseData);
+                responseData.activatedDevices = activatedDevices;
+                console.log("Activated devices  " + activatedDevices);
+                resp.header("Authorization", req.params.token).status(200).send(responseData);
+            })
         })
-    })
-        .catch(function (error) {
+        .catch(function(error) {
             console.log("Error occured while querying devices " + err);
         })
 
 
 })
 
-app.post("/login", authentication.authenticateUser, function (req, resp, next) {
+app.post("/login", authentication.authenticateUser, function(req, resp, next) {
 
-    appdb.enterpriseService.queryEnterpriseByFilter("email", req.body.email).then(function (enterpriseDetails) {
+    appdb.enterpriseService.queryEnterpriseByFilter("email", req.body.email).then(function(enterpriseDetails) {
         console.log("Enterprise queried successfully." + enterpriseDetails);
-        appdb.deviceService.queryDeviceByFilter("enterpriseId", enterpriseDetails._id).then(function (devices) {
+        appdb.deviceService.queryDeviceByFilter("enterpriseId", enterpriseDetails._id).then(function(devices) {
             console.log("Total  devices : " + devices);
             var responseData = _.pick(enterpriseDetails, 'firstname', 'lastname');
             responseData.totalDevices = devices.length;
@@ -239,30 +239,30 @@ app.post("/login", authentication.authenticateUser, function (req, resp, next) {
             responseData.activatedDevices = activatedDevices;
             console.log("Activated devices  " + activatedDevices);
             resp.header("Authorization", req.params.token).status(200).send(responseData);
-        }).catch(function (err) {
+        }).catch(function(err) {
             console.log("Error occured while querying devices " + err);
         });
     });
 });
 
 
-app.delete("/logout", function (req, resp, next) {
+app.delete("/logout", function(req, resp, next) {
     // console.log("logout");
     var bearerJwt = req.get("Bearer");
-    appdb.tokenService.deleteToken(bearerJwt).then(function (count) {
+    appdb.tokenService.deleteToken(bearerJwt).then(function(count) {
         //console.log("count : "+count.result.n);
         if (count.result.n === 1) {
             resp.status(200).send("Logged out successfully.");
         }
         //console.log(result);
         resp.status(401).send("Unauthorized user, please login again.");
-    }).catch(function (err) {
+    }).catch(function(err) {
         resp.status(500).send("Sorry !!!! Some error occured while creating device.Please try again.");
     });
 });
 
 var staticMiddlewarePrivate = express['static'](__dirname + '/app/partials');
-app.get('/private/*', authentication.authenticateRequired, function (req, res, next) {
+app.get('/private/*', authentication.authenticateRequired, function(req, res, next) {
     console.log('**** Private ****' + req.url);
     req.url = req.url.replace(/^\/private/, '');
     staticMiddlewarePrivate(req, res, next);
@@ -272,20 +272,22 @@ app.get('/private/*', authentication.authenticateRequired, function (req, res, n
 app.use(express.static(__dirname + "/app/public"));
 
 
-var connect = mongoose.connect('mongodb://localhost:27017/iotaccdb', function () {
+
+var mongo_uri = process.env.MONGODB_URI || "mongodb://localhost:27017"
+var connect = mongoose.connect(mongo_uri + '/iotaccdb', function() {
     mongoose.connection.db.dropDatabase()
     console.log("Database is dropped succesfully");
 });
 
-connect.then(function (res) {
-    http.listen(PORT, function (error, success) {
+connect.then(function(res) {
+    http.listen(PORT, function(error, success) {
         if (error) {
             console.log("server startup failed");
         } else {
             console.log("server is started at port " + PORT + " press [ctrl+c] to exit!!");
         }
 
-    }).catch(function (err) {
+    }).catch(function(err) {
         console.log(err);
     });
 });
--- app/modules/mail/sendmail.js
+++ app/modules/mail/sendmail.js
@@ -4,6 +4,13 @@ var mailjet = require('node-mailjet').connect('7c6c927b44d43f8d213413548369d4ed'
     //    .connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
 
 var send = function(mailObj) {
+    var env = process.env.NODE_ENV || 'development';
+    if (env === 'production') {
+        urlPref = 'https://iot-one.herokuapp.com';
+
+    } else {
+        urlPref = 'http://localhost:9000'
+    }
     var request = mailjet.post("send").request({
         "FromEmail": "mail2abhic@gmail.com",
         "FromName": "IOTAPP Support Team",
@@ -15,7 +22,7 @@ var send = function(mailObj) {
         }],
         "Vars": {
             "firstname": mailObj.name,
-            "link": "http://localhost:9099/enterprise/activate?activateId=" + mailObj.activateId
+            "link": urlPref + "/enterprise/activate?activateId=" + mailObj.activateId
         }
     });
     return request;
